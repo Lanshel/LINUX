d3559f67fb3dcfc9dc6392448c8bcad1693a68bb
Landysh Khamidullina <73118572+Lanshel@users.noreply.github.com>
2021-06-07 00:12:13 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir Landysh
+cd Landysh
+mkdir -p test/test-1/test-2
+cd  "C:\Users\dima\Landysh\test\Landysh\test\test-1\test-2"
+touch file1.txt file2.txt file3.txt
+mkdir {5,66,88}
+ls
+
+
+
